b7e54422a11d9368d30b42e74a43c6966a3d789e
Liu Ho Man <hmliu6@gmail.com>
2017-12-27 21:21:00 +0800

    Add sample OpenCV code for compilation testing and script for Mac OS compilation

---
+++ OpenCV-compile.sh
@@ -0,0 +1,12 @@
+
+
+#!/bin/bash
+
+if [ $# -eq 0 ]; then
+    echo "Input cpp filename for compilation in first argument"
+    exit 0
+fi
+
+g++ "${1}.cpp" -o "${1}.o" `pkg-config --cflags --libs /usr/local/Cellar/opencv/3.4.0/lib/pkgconfig/opencv.pc`
+
+./"${1}.o"
