22239585c7c263b17c8d533dbe9f9bc0673af5cc
Liu Ho Man <hmliu6@gmail.com>
2017-12-28 17:02:02 +0800

    Filter out all pixels out of depth range

--- filtering.cpp
+++ filtering.cpp
@@ -45,6 +45,7 @@ char* pathParser(const char *fileDir){
         parsedPath[i] = fileDir[i];
     
     parsedPath[strlen(fileDir)] = pathSlash;
+    parsedPath[strlen(fileDir)+1] = '\0';
     
     std::strcat(parsedPath, "test");
     sprintf(buffer,"%d",imageNumber);
@@ -58,6 +59,47 @@ char* pathParser(const char *fileDir){
     return parsedPath;
 }
 
+void boundingArea(Mat rawImage, Mat image, int **whitePoints){
+    
+}
+
+void preFiltering(char *filePath, int upperColorRange, int lowerColorRange){
+	cv::Mat rawImage, image;
+	rawImage = imread(filePath, 1);
+
+	if (!rawImage.data)
+		cout << "Cannot find document!!!" << endl;
+	else{
+        int *whitePoints[2], pointCount = 0;
+        whitePoints[0] = (int *)malloc(1000 * sizeof(int));
+        whitePoints[1] = (int *)malloc(1000 * sizeof(int));
+        // After opening files, convert to greyscale and pass to processing function
+		cvtColor(rawImage, image, COLOR_BGR2GRAY);
+        // Filter out unrelated pixels
+        for(int j=0; j<image.cols; j++){
+            for(int i=0; i<image.rows; i++){
+                if(i >= image.cols/2)
+                    image.at<uchar>(i, j) = 0;
+                else if(image.at<uchar>(i, j) <= lowerColorRange)
+                    image.at<uchar>(i, j) = 0;
+                else if(image.at<uchar>(i, j) >= upperColorRange)
+                    image.at<uchar>(i, j) = 0;
+                else{
+                    image.at<uchar>(i, j) = 255;
+                    whitePoints[0][pointCount] = i;
+                    whitePoints[1][pointCount] = j;
+                    pointCount += 1;
+                }
+            }
+        }
+        // for(int i=0; i<pointCount; i++)
+        //     cout << "{ " << whitePoints[0][i] << ", " << whitePoints[1][i] << " }" << endl;
+        boundingArea(rawImage, image, whitePoints);
+		imshow("Test", image);
+		cvWaitKey(0);
+	}
+}
+
 int main(int argc, char **argv){
     int upperColor, lowerColor;
     // Pass by reference
@@ -65,6 +107,7 @@ int main(int argc, char **argv){
 
     char* filePath = pathParser(imageDir);
     cout << filePath << endl;
+    preFiltering(filePath, upperColor, lowerColor);
 
     return 0;
 }
